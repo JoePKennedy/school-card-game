5fa11add4ab9dc9905316a6f71e47d38a48327b9
JoePKennedy <34372026+JoePKennedy@users.noreply.github.com>
2017-12-13 11:49:15 +0000

    Add files via upload

--- __main__.py
+++ __main__.py
@@ -48,6 +48,7 @@ class Menu(Button):
     def __init__(self):
         Button.__init__(self, string="", width=0)
         self.running = True
+        self.next_screen = 0
 
     current_selection = 1
     """
@@ -57,6 +58,8 @@ class Menu(Button):
     def run_menu(self):
         key_pressed = pygame.key.get_pressed()
         for event in pygame.event.get():
+            if self.running is False:
+                return self.next_screen
             # Every time something happens
             if event.type == QUIT:
                 self.running = False
@@ -74,6 +77,12 @@ class Menu(Button):
             if key_pressed[K_RETURN]:
                 if self.current_selection is 2:
                     self.running = False
+                    self.next_screen = 2
+                if self.current_selection is 0:
+                    self.running = False
+                    self.next_screen = 0
+            else:
+                return "continue"
 
         screen.fill(Grey)
         # Make the screen grey
@@ -97,19 +106,108 @@ class Menu(Button):
         pygame.display.flip()
 
 
-class PacksMenu(Button):
+class Pack(Button):
     def __init__(self):
         Button.__init__(self, string="", width=0)
         self.running = True
+        self.next_screen = -1
 
     current_selection = 0
 
+    def run_menu(self):
+        key_pressed = pygame.key.get_pressed()
+        for event in pygame.event.get():
+            # Every time something happens
+            if event.type == QUIT:
+                self.running = False
+                pygame.quit()
+                # If that something is they want to quit, end the game
+            if key_pressed[K_LEFT]:
+                if self.current_selection != 0:
+                    self.current_selection -= 1
+                    print(self.current_selection)
+                    # Increments option by 1
+            if key_pressed[K_RIGHT]:
+                if self.current_selection != 2:
+                    self.current_selection += 1
+                    print(self.current_selection)
+            if key_pressed[K_RETURN]:
+                if self.current_selection is 2:
+                    self.running = False
+                    return 2
+
+        screen.fill(Grey)
+        # Make the screen grey
+
+        purchase_button = Button("Buy Packs!", 0.40)
+        open_button = Button("Open Packs!", 0.70)
+        purchase_button.draw()
+        open_button.draw()
+        # Initialising the buttons then drawing them
+        pygame.display.update()
+
+
+class Deck(Button):
+    def __init__(self):
+        Button.__init__(self, string="", width=0)
+        self.running = True
+        self.next_screen = -1
+
+    current_selection = 0
+
+    def run_menu(self):
+        key_pressed = pygame.key.get_pressed()
+        for event in pygame.event.get():
+            # Every time something happens
+            if event.type == QUIT:
+                self.running = False
+                pygame.quit()
+                # If that something is they want to quit, end the game
+            if key_pressed[K_LEFT]:
+                if self.current_selection != 0:
+                    self.current_selection -= 1
+                    print(self.current_selection)
+                    # Increments option by 1
+            if key_pressed[K_RIGHT]:
+                if self.current_selection != 2:
+                    self.current_selection += 1
+                    print(self.current_selection)
+            if key_pressed[K_RETURN]:
+                if self.current_selection is 2:
+                    self.running = False
+                    return 2
+
+        screen.fill(Grey)
+        # Make the screen grey
+
+        create_button = Button("Create a deck!", 0.40)
+        edit_button = Button("Edit a Deck!", 0.70)
+        create_button.draw()
+        edit_button.draw()
+        # Initialising the buttons then drawing them
+        pygame.display.update()
+
 main_menu = Menu()
+pack_menu = Pack()
+deck_menu = Deck()
 menu_running = True
+pack_running = False
+deck_running = False
+
 while game_running:
-    x = main_menu.run_menu()
-    if x is False:
-        break
+    while menu_running:
+        x = main_menu.run_menu()
+        if main_menu.running is False:
+            menu_running = False
+            if main_menu.next_screen is 0:
+                deck_running = True
+            if main_menu.next_screen is 2:
+                pack_running = True
+    while pack_running:
+        pack_menu.run_menu()
+    while deck_running:
+        deck_menu.run_menu()
+
 
 
 print("See ya")
